53bb13205af6f72f491edd5a7f65f7ade308f690
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-04-25 10:59:16 +0200

    exercise

--- Exercisee.java
+++ Exercisee.java
@@ -1,3 +1,4 @@
+import package1.Car;
 import package1.TicTac;
 import package2.Days;
 import package2.Numbers;
@@ -8,27 +9,32 @@ public class Exercisee {
         System.out.println("hello visma");
     }
 
-    public void yourMethod () {
+    public void yourMethod() {
         System.out.println("capperacci");
     }
+
     public static void main(String[] args) {
         System.out.println("Welcome to my first exercise");
         Numbers numbers = new Numbers();
         TicTac ticTac = new TicTac();
         ticTac.printString("fist string", "second string");
         Days days = new Days();
+        Car car = new Car();
+
         myMethod();
 
         Exercisee exe = new Exercisee();
         exe.yourMethod();
 
-        System.out.println(days.weekDays.length);
+        /*System.out.println(days.weekDays.length);
         System.out.println(days.weekDays[3]);
         System.out.println(numbers.arrayOfInt[2]);
         System.out.println(ticTac.ticTacGame[2][2]);
         System.out.println(ticTac.trainingTable[1][0]);
+        */
         ticTac.printLoopString("hi", "hello", "vismass");
         // System.out.println(ticTac.printLoopString("ho", "hi"));
         // hello
+        System.out.println(car.name + ", brand : " + car.brand + ", age : " + car.age);
     }
 }
---
+++ package1/Car.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class Car {
+    public String name = "Volvo2";
+    public String brand = "Volvo";
+    public int age  = 3;
+
+
+
+ }
